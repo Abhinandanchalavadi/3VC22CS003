fbac5b61e75f9784795fc29d42b772fc7bdbce07
Abhinandanchalavadi <154590652+Abhinandanchalavadi@users.noreply.github.com>
2024-03-18 12:54:12 +0530

    Update prg1.c

--- prg1.c
+++ prg1.c
@@ -15,3 +15,10 @@ $ git commit -m " I am in 12b branch master changes 1 "
 $ git log --oneline
 $ git revert ec9d05e
 12
+ $ git checkout master
+$ echo "I am in 8a" >> lab8a.txt
+git add .
+$ git commit -m " I am in 8a branch master "
+$ git log
+$ git show f7b46493e18b1dd58703830637ae9d3e615adbde
+  8
