51e7aa50b5dfe652177063c5de4fd346f133c7a1
Abhinandanchalavadi <154590652+Abhinandanchalavadi@users.noreply.github.com>
2024-03-18 12:45:14 +0530

    Update prg1.c

--- prg1.c
+++ prg1.c
@@ -1,1 +1,17 @@
-hello
+$ git checkout master
+$ vi 12a.txt
+git add .
+$ git commit -m " I am in 12a branch master "
+$ vi 12b.txt
+git add .
+$ git commit -m " I am in 12b branch master "
+$ vi 12a.txt
+git add .
+$ git commit -m " I am in 12a branch master changes 1"
+$ vi 12b.txt
+.
+git add .
+$ git commit -m " I am in 12b branch master changes 1 "
+$ git log --oneline
+$ git revert ec9d05e
+12
